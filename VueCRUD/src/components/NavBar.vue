1537f165bc08d72915cf9f79da08560e9d8ebd3f
Shr4528 <Shr4528@gmail.com>
2022-10-29 13:59:12 +0300

    test

---
+++ VueCRUD/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar">
+        <div @click="$router.push('/')">Vue £</div>
+        <div class="navbar__btns">
+            <my-button @click="$router.push('/posts')">Posts</my-button>
+            <my-button style='margin-left: 20px' @click="$router.push('/about')">About</my-button>
+            <my-button style='margin-left: 20px' @click="$router.push('/store')">store</my-button>
+            <my-button style='margin-left:20px' @click="$router.push('/composition')">Composition</my-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: rgb(147, 24, 24);
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
